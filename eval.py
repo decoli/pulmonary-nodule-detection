4e611cc5a2995839a866e4f19267482769d58433
unknown <shirui.zhang.china@gmail.com>
2020-06-05 20:02:46 +0800

    11

--- eval.py
+++ eval.py
@@ -415,10 +415,15 @@ def test_net(save_folder, net, cuda, dataset, transform, top_k,
             if args.debug:
                 cv2.imwrite('test/test.png', img_original, [int(cv2.IMWRITE_PNG_COMPRESSION), 0])
                 cv2.imwrite('test/test.jpg', img_original, [int(cv2.IMWRITE_JPEG_QUALITY), 100])
-                print(boxes[:, 0])
-                print(boxes[:, 1])
-                print(boxes[:, 2])
-                print(boxes[:, 3])
+
+                for k in range(boxes.shape[0]):
+                    point_left_up = (int(boxes[k, 0]), int(boxes[k, 1]))
+                    point_right_down = (int(boxes[k, 2]), int(boxes[k, 3]))
+
+                    cv2.rectangle(img_original, point_left_up, point_right_down, (0, 0, 255), 1)
+                    
+                    cv2.imwrite('test/test.png', img_original, [int(cv2.IMWRITE_PNG_COMPRESSION), 0])
+                    cv2.imwrite('test/test.jpg', img_original, [int(cv2.IMWRITE_JPEG_QUALITY), 100])
 
         print('im_detect: {:d}/{:d} {:.3f}s'.format(i + 1,
                                                     num_images, detect_time))
