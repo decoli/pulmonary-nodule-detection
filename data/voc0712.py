f551295632b8a120c1f69f2758dd0837350c4a41
unknown <shirui.zhang.china@gmail.com>
2020-06-06 14:35:52 +0800

    11

--- data/voc0712.py
+++ data/voc0712.py
@@ -24,6 +24,27 @@ VOC_CLASSES = (  # always index 0
     'motorbike', 'person', 'pottedplant',
     'sheep', 'sofa', 'train', 'tvmonitor')
 
+    # 1.'aeroplane',
+    # 2.'bicycle',
+    # 3.'bird',
+    # 4.'boat',
+    # 5.'bottle',
+    # 6.'bus',
+    # 7.'car',
+    # 8.'cat',
+    # 9.'chair',
+    # 10.'cow',
+    # 11.'diningtable',
+    # 12.'dog',
+    # 13.'horse',
+    # 14'motorbike',
+    # 15.'person',
+    # 16.'pottedplant',
+    # 17.'sheep',
+    # 18.'sofa',
+    # 19.'train',
+    # 20'tvmonitor'
+
 # note: if you used our download scripts, this should be right
 VOC_ROOT = osp.join(HOME, "data/VOCdevkit/")
 
