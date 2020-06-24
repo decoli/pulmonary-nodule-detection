a9d10d2e301c13906c8c9f3d0013de86144dd6a6
unknown <shirui.zhang.china@gmail.com>
2020-06-24 14:13:38 +0800

    11

--- data/voc0712.py
+++ data/voc0712.py
@@ -23,7 +23,8 @@ VOC_CLASSES = (  # always index 0
     # 'cow', 'diningtable', 'dog', 'horse',
     # 'motorbike', 'person', 'pottedplant',
     # 'sheep', 'sofa', 'train', 'tvmonitor')
-    'nodule',)
+    'nodule',
+    'nonnodule',)
 
     # 1.'aeroplane',
     # 2.'bicycle',
